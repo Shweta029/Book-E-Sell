2d2469a8851d5678c56871011b3a9328e233c923
Shweta029 <89931849+Shweta029@users.noreply.github.com>
2022-07-11 22:48:33 +0530

    Add files via upload

---
+++ BookStore.Api/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿using BookStore.Models.DataModels;
+using BookStore.Models.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BookStore.Api.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserRepository _repository = new UserRepository();
+
+        [HttpGet]
+        [Route("list")]
+        public IActionResult GetUsers(int pageIndex = 1, int pageSize = 10, string keyword = "")
+        {
+            ListResponse<User> response = _repository.GetUsers(pageIndex, pageSize, keyword);
+            ListResponse<UserModel> users = new ListResponse<UserModel>()
+            {
+                Results = response.Results.Select(u => new UserModel(u)),
+                TotalRecords = response.TotalRecords,
+            };
+
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            User user = _repository.GetUser(id);
+            if (user == null)
+                return NotFound();
+
+            UserModel userModel = new UserModel(user);
+            return Ok(userModel);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult UpdateUser(UserModel model)
+        {
+            if (model != null)
+                return BadRequest();
+
+            User user = new User()
+            {
+                Id = model.Id,
+                Firstname = model.Firstname,
+                Lastname = model.Lastname,
+                Email = model.Email,
+                Roleid = model.Roleid,
+            };
+
+            user = _repository.UpdateUser(user);
+            return Ok(user);
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public IActionResult DeleteUser(int id)
+        {
+            bool isDeleted = _repository.DeleteUser(id);
+            return Ok(isDeleted);
+        }
+
+        [HttpGet]
+        [Route("roles")]
+        [ProducesResponseType(typeof(ListResponse<RoleModel>), (int)System.Net.HttpStatusCode.OK)]
+        public IActionResult GetRoles(string keyword, int pageIndex = 1, int pageSize = 10)
+        {
+            var roles = _repository.GetRoles(pageIndex, pageSize, keyword);
+            ListResponse<RoleModel> listResponse = new ListResponse<RoleModel>()
+            {
+                Results = roles.Results.Select(c => new RoleModel(c)),
+                TotalRecords = roles.TotalRecords,
+            };
+
+            return Ok(listResponse);
+        }
+    }
+}
---
+++ BookStore.Models/Models/RoleModelcs.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Models.DataModels;
+
+namespace BookStore.Models.Models
+{
+    public class RoleModel
+    {
+        public RoleModel() { }
+
+        public RoleModel(Role role)
+        {
+            Id = role.Id;
+            Name = role.Name;
+
+        }
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
