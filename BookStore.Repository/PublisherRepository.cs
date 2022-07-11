85b90f725726ba700dff3247e9b1670faaa3f845
Shweta029 <89931849+Shweta029@users.noreply.github.com>
2022-07-11 22:51:28 +0530

    Add files via upload

---
+++ BookStore.Repository/PublisherRepository.cs
@@ -0,0 +1,54 @@
+﻿using BookStore.Models.DataModels;
+using BookStore.Models.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Repository
+{
+    public class PublisherRepository  : BaseRepository
+    {
+        public ListResponse<Publisher> GetPublishers(int pageIndex, int pageSize, string keyword)
+        {
+            keyword = keyword?.ToLower()?.Trim();
+            var query = _context.Publishers.Where(c => keyword == null || c.Name.ToLower().Contains(keyword)).AsQueryable();
+            int totalReocrds = query.Count();
+            List<Publisher> categories = query.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+
+            return new ListResponse<Publisher>()
+            {
+                Results = categories,
+                TotalRecords = totalReocrds,
+            };
+        }
+
+        public Publisher GetPublisher(int id)
+        {
+            return _context.Publishers.FirstOrDefault(c => c.Id == id);
+        }
+
+        public Publisher AddPublisher(Publisher category)
+        {
+            var entry = _context.Publishers.Add(category);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public Publisher UpdatePublisher(Publisher category)
+        {
+            var entry = _context.Publishers.Update(category);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public bool DeletePublisher(int id)
+        {
+            var category = _context.Publishers.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+                return false;
+
+            _context.Publishers.Remove(category);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
